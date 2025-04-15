28dba4b386667a020c1a070996064581fb66d859
Krzysztof <66274761+malezjaa@users.noreply.github.com>
2025-04-15 21:43:22 +0200

    feat: make client work

--- vscode/client/extension.ts
+++ vscode/client/extension.ts
@@ -31,51 +31,5 @@ import { activate as lspActivate } from "./lsp";
 export const outputChannel = window.createOutputChannel("Brim Extension");
 
 export async function activate(context: ExtensionContext) {
-  outputChannel.appendLine("Activating Brim Language Server extension...");
-
   lspActivate();
-
-  // I know this is bad, but only for development
-  const command =
-    "C:\\dev\\brim-projects\\ide\\target\\debug\\brim-language-server.exe";
-
-  const run: Executable = {
-    command,
-    options: {
-      env: {
-        ...process.env,
-        RUST_LOG: "debug",
-      },
-    },
-  };
-
-  const serverOptions: ServerOptions = {
-    run,
-    debug: run,
-  };
-  let clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "brim" }],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-    outputChannel: outputChannel,
-  };
-
-  client = new LanguageClient(
-    "brim-language-server",
-    "brim language server",
-    serverOptions,
-    clientOptions,
-  );
-
-  await client.start();
-  outputChannel.appendLine("Brim Language Server started.");
-}
-
-export function deactivate(): Thenable<void> | undefined {
-  outputChannel.appendLine("Deactivating Brim Language Server extension...");
-  if (!client) {
-    return undefined;
-  }
-  return client.stop();
 }
--- vscode/client/lsp.ts
+++ vscode/client/lsp.ts
@@ -1,35 +1,83 @@
-import * as vscode from 'vscode';
-import { outputChannel } from './extension';
-import { LanguageClient } from 'vscode-languageclient/node';
+import * as vscode from "vscode";
+import { outputChannel } from "./extension";
+import {
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 export let client: LanguageClient | null = null;
 
 export async function activate() {
+  try {
     const path = getExecPath();
 
     if (!path) {
-        await stopClient();
-        return 
+      await stopClient();
+      return;
     }
 
-    try {
-        const client = await startClient(path);
-    }
+    const newClient = await startClient(path);
+    stopClient();
+    client = newClient;
+  } catch (err) {
+    vscode.window.showWarningMessage(
+      `Failed to run Brim LSP${err instanceof Error ? `: ${err.message}` : ""}`,
+    );
+  }
 }
+
+const startClient = async (path: string): Promise<LanguageClient> => {
+  const run: Executable = {
+    command: path,
+    options: {
+      env: {
+        ...process.env,
+        RUST_LOG: "debug",
+      },
+    },
+  };
+
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "file", language: "brim" },
+      { scheme: "untitled", language: "brim" },
+    ],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    outputChannel: outputChannel,
+  };
+
+  const langClient = new LanguageClient(
+    "brim-language-server",
+    "brim language server",
+    serverOptions,
+    clientOptions,
+  );
+
+  await langClient.start();
+
+  return langClient;
+};
 
 const getExecPath = (): string => {
-    const config = vscode.workspace.getConfiguration("brim");
-    const path = config.get("path") as string | undefined;
-    if (!path) {
-        throw new Error("TODO: No LSP path configured.");
-    }
-    return path;
-}
+  const config = vscode.workspace.getConfiguration("brim");
+  const path = config.get("lspPath") as string | undefined;
+  if (!path) {
+    throw new Error("No LSP path configured.");
+  }
+  return path;
+};
 
-
-const  stopClient = async (): Promise<void> =>{
-    if (!client) return;
-    const oldClient = client;
-    client = null;
-    await oldClient.stop();
-    await oldClient.dispose();
-}
+const stopClient = async (): Promise<void> => {
+  if (!client) return;
+  const oldClient = client;
+  client = null;
+  await oldClient.stop();
+  await oldClient.dispose();
+};
